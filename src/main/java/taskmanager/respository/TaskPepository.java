731c16591f6ad1f2d822686fd51866587fcc4bc8
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-06 09:39:53 +0900

    修正

--- src/main/java/taskmanager/respository/TaskPepository.java
+++ src/main/java/taskmanager/respository/TaskPepository.java
@@ -1,8 +1,9 @@
-package taskmanager.repository;
+package taskmanager.respository;
 
-import com.example.taskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+import taskmanager.model.Task;
+
+public interface TaskPepository extends JpaRepository<Task, Long> {
     // 必要に応じてカスタムクエリを追加できます
 }
