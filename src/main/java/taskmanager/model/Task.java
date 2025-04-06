ddcdb460fee56fb2cdb0adbddf5aed0d9e38587b
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-06 09:40:32 +0900

    修正

---
+++ src/main/java/taskmanager/model/Task.java
@@ -0,0 +1,41 @@
+package taskmanager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String taskName;
+    private boolean completed;
+
+    // GetterとSetter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
