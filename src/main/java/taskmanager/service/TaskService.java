de713186c36c1ab84f5ea151bf194533e152a41f
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-06 09:40:02 +0900

    修正

---
+++ src/main/java/taskmanager/service/TaskService.java
@@ -0,0 +1,44 @@
+package taskmanager.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import taskmanager.model.Task;
+import taskmanager.respository.TaskRepository;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    // タスクの取得
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    // タスクの作成
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    // タスクの更新
+    public Task updateTask(Long id, Task taskDetails) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            Task existingTask = task.get();
+            existingTask.setTaskName(taskDetails.getTaskName());
+            existingTask.setCompleted(taskDetails.isCompleted());
+            return taskRepository.save(existingTask);
+        }
+        return null;
+    }
+
+    // タスクの削除
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
