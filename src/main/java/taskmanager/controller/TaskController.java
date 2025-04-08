ad8b1043e8cf52c5ef36d95d3c45d79b432d7f13
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-08 21:15:43 +0900

    TaskControllerの修正

--- src/main/java/taskmanager/controller/TaskController.java
+++ src/main/java/taskmanager/controller/TaskController.java
@@ -3,6 +3,7 @@ package taskmanager.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;  // 追加
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import taskmanager.service.TaskService;
 
 @RestController
 @RequestMapping("/tasks")
+@CrossOrigin(origins = "http://localhost:3000")  // 追加: Reactアプリケーションがアクセスできるように設定
 public class TaskController {
 
     @Autowired
