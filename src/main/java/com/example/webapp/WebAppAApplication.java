a3dc4b2f626e73a8c4791b7eada4e5962f5f5a62
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-09 21:27:32 +0900

    新規作成

---
+++ src/main/java/com/example/webapp/WebAppAApplication.java
@@ -0,0 +1,12 @@
+package com.example.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppAApplication.class, args);
+    }
+}
