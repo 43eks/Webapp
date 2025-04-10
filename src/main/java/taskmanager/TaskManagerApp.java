96b9924f0ff6739cb8f5da07a822239fb853da51
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-10 19:24:05 +0900

    修正

--- src/main/java/taskmanager/TaskManagerApp.java
+++ src/main/java/taskmanager/TaskManagerApp.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "taskmanager.repository")  // Repositoryパッケージをスキャン
-@EntityScan(basePackages = "taskmanager.model")  // Entityパッケージをスキャン
+@EnableJpaRepositories(basePackages = "taskmanager.repository")  // Repository パッケージのスペル修正
+@EntityScan(basePackages = "taskmanager.model")  // Entity パッケージをスキャン
 public class TaskManagerApp {
 
     public static void main(String[] args) {
