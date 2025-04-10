1c74a84836b059f50edfffe158183482e11c3a5d
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-10 19:41:47 +0900

    フォルダ名など修正

--- src/main/java/taskmanager/respository/TaskPepository.java
+++
@@ -1,9 +0,0 @@
-package taskmanager.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import taskmanager.model.Task;
-
-public interface TaskPepository extends JpaRepository<Task, Long> {
-    // 必要に応じてカスタムクエリを追加できます
-}
