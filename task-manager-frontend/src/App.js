fb00f6ccb179490e4e9e9deda8832f6a299a6cdf
komagenn <89065599+komagenn@users.noreply.github.com>
2025-04-21 20:51:57 +0900

    修正

--- task-manager-frontend/src/App.js
+++ task-manager-frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import KnowledgeList from './pages/knowledgeList';  // ナレッジ一覧ページ
-import Createknowledge from './pages/Createknowledge';  // ナレッジ作成ページ
-import Editknowledge from './pages/Editknowledge';  // ナレッジ編集ページ
-import Viewknowledge from './pages/Viewknowledge';  // ナレッジ詳細表示ページ
-import Home from './pages/Home';  // 機能選択ページ
-import TaskList from './pages/TaskList';  // タスク一覧ページ
-import CreateTask from './pages/CreateTask';  // タスク追加ページ
 
-// ✅ ここに追加（どのファイルからでも使えるように後でexportしてもOK）
+import KnowledgeList from './pages/KnowledgeList';        // ナレッジ一覧
+import CreateKnowledge from './pages/CreateKnowledge';    // ナレッジ作成
+import EditKnowledge from './pages/EditKnowledge';        // ナレッジ編集
+import ViewKnowledge from './pages/ViewKnowledge';        // ナレッジ詳細
+
+import Home from './pages/Home';                          // ホーム画面
+
+import TaskList from './pages/TaskList';                  // タスク一覧
+import CreateTask from './pages/CreateTask';              // タスク追加
+import TaskDetail from './pages/TaskDetail';              // タスク詳細 ← 追加！
+
+// ✅ 共通APIエンドポイント
 export const API_BASE_URL = 'http://localhost:8080';
 
 function App() {
@@ -23,14 +27,17 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+
           {/* ナレッジ機能 */}
           <Route path="/knowledges" element={<KnowledgeList />} />
-          <Route path="/knowledges/create" element={<Createknowledge />} />
-          <Route path="/knowledges/:id/edit" element={<Editknowledge />} />
-          <Route path="/knowledges/:id" element={<Viewknowledge />} />
+          <Route path="/knowledges/create" element={<CreateKnowledge />} />
+          <Route path="/knowledges/:id/edit" element={<EditKnowledge />} />
+          <Route path="/knowledges/:id" element={<ViewKnowledge />} />
+
           {/* タスク機能 */}
           <Route path="/tasks" element={<TaskList />} />
           <Route path="/tasks/create" element={<CreateTask />} />
+          <Route path="/tasks/:id" element={<TaskDetail />} /> {/* ← これが警告解消の鍵！ */}
         </Routes>
       </div>
     </Router>
