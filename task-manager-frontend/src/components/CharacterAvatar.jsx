fc92985ed943e6c197c72a63091a13bbbdbf75d6
komagenn <89065599+komagenn@users.noreply.github.com>
2025-05-23 21:25:29 +0900

    新規作成

---
+++ task-manager-frontend/src/components/CharacterAvatar.jsx
@@ -0,0 +1,13 @@
+// src/components/CharacterAvatar.jsx
+import React from 'react';
+import './CharacterAvatar.css';
+
+function CharacterAvatar() {
+  return (
+    <div className="character-avatar">
+      <img src="/character/mocha.png" alt="キャラクター" />
+    </div>
+  );
+}
+
+export default CharacterAvatar;
